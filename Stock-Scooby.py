01036b985ac05ee563cb167292c38dc0446f4d38
krisalpha <choprakrishan61@gmail.com>
2016-10-26 00:02:05 +0530

    In This commit i have successfully made a pandas dataframe indexed on basis of date. The index date starts and ends at dates supplied by the user.Then another dataframe is created to read csv file, here the default indexing is changed from number to date so that it can successfully join with the dataframe created before left join is by first frame on second. dropna function is used to drop any entries containing nan values.
    Signed-off-by: krisalpha <choprakrishan61@gmail.com>

--- Stock-Scooby.py
+++ Stock-Scooby.py
@@ -11,18 +11,52 @@ def get_max_close(path):
     df = pandas.read_csv(path)
     return df['Close'].max()
 
-def plot_high_values(path):
+def plot_values(path):
     df = pandas.read_csv(path)
-    print df['High']
-    df['High'].plot()
+    #print df['High'] beware of [[DOUBLE BRACKETS]] for multiple values
+    df[['High', 'Adj Close']].plot()
     plt.show()
 
+def get_mean_volume(path):
+    """Returns the mean volume for stock
+    """
+    df = pandas.read_csv(path)
+    return df['Volume'].mean()
+
+def make_dataframe(path):
+    start_date = '2014-01-01'
+    end_date = '2016-10-07'
+    dates = pandas.date_range(start_date, end_date)
+    #This is an empty DataFrame which is indexed by Date
+    df1 = pandas.DataFrame(index = dates)
+
+    #ReadSPY data into temporary dataframe
+    #indexes column by Date,, parse_dates = True allows the dataframe to be
+    #be indexed as DateTime value objects,,
+    dfSENSEX = pandas.read_csv(path, index_col = "Date", parse_dates = True)
+
+    #now lets join the dataframes
+    dfSENSEX = pandas.read_csv(path, index_col = "Date",parse_dates = True,
+                        usecols = ['Date', 'Adj Close'],
+                        na_values = ['nan'])
+
+    #dropna() drops any nan values and this join  is by default left join,
+    df1 = df1.join(dfSENSEX)
+    df1 = df1.dropna()
+    print df1
+
 def test_run():
-    """ my driver function """
-    for symbol in ['ITC' , 'TCS']:
+    """ my driver function
+    """
+    path = "./Data/SENSEX/TCS.csv"
+    make_dataframe(path)
+
+    """for symbol in ['ITC' , 'TCS']:
         print "Max Close is"
         path = "./Data/SENSEX/{}.csv".format(symbol)
-        print symbol, get_max_close(path)
+        #print symbol, get_mean_volume(path)
+        plot_values(path)
+    """
 
 if __name__ == "__main__":
     test_run()
