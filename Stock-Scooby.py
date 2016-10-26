0a776777c368f33eeaa3516b1e6c4f47e9961362
krisalpha <choprakrishan61@gmail.com>
2016-10-26 12:59:32 +0530

    In this commit, two utility functions symbol_to_path and get_data_frame have been added. In get_data_frame takes S&P SENSEX indicator as base for all stocks. Then other stocks are also joined in this dataframe.
    Signed-off-by: krisalpha <choprakrishan61@gmail.com>

--- Stock-Scooby.py
+++ Stock-Scooby.py
@@ -1,3 +1,4 @@
+import os
 import pandas
 import matplotlib.pyplot as plt
 
@@ -23,7 +24,12 @@ def get_mean_volume(path):
     df = pandas.read_csv(path)
     return df['Volume'].mean()
 
-def make_dataframe(path, start_date, end_date):
+def symbol_to_path(symbol, base_dir = "./Data/SENSEX"):
+    """Returns CSV file path for given symbol """
+    return os.path.join(base_dir, "{}.csv".format(str(symbol)))
+
+
+"""def make_dataframe(path, start_date, end_date):
     dates = pandas.date_range(start_date, end_date)
     #This is an empty DataFrame which is indexed by Date
     df1 = pandas.DataFrame(index = dates)
@@ -39,24 +45,56 @@ def make_dataframe(path, start_date, end_date):
                         na_values = ['nan'])
 
     #dropna() drops any nan values and this join  is by default left join,
-    df1 = df1.join(dfSENSEX, how = 'inner')
+    #df1 = df1.join(dfSENSEX, how = 'inner')
     #df1 = df1.dropna()
+    #print df1
+    #reading more stocks
+
+    symbols = ['TCS', 'ITC']
+    for symbol in  symbols:
+        df_temp = pandas.read_csv(symbol_to_path(symbol),
+                               index_col = "Date", parse_dates = True,
+                               usecols = ['Date', 'Adj Close'],na_values = ['nan'])
+
+        df_temp = df_temp.rename(columns={'Adj Close' : symbol})
+        df1 = df1.join(df_temp, how = 'inner')
     print df1
+"""
+
+def get_data_frame(symbols, dates):
+
+    df = pandas.DataFrame(index = dates)
+
+    #sensex file acts as base;  this is the index file for sensex
+    if 'SENSEX' not in symbols:
+        symbols.insert(0, 'SENSEX')
+
+    for symbol in  symbols:
+        df_temp = pandas.read_csv(symbol_to_path(symbol),
+                               index_col = "Date", parse_dates = True,
+                               usecols = ['Date', 'Adj Close'],na_values = ['nan'])
+
+        df_temp = df_temp.rename(columns={'Adj Close' : symbol})
+        if symbol != 'SENSEX':
+            df = df.join(df_temp)
+        else:
+            df = df.join(df_temp, how = 'inner')
+        """
+            This is important as SENSEX csv file is base for all stocks
+             we have an entry for this whenever SENSEX trades,
+             but for any compamy listed on sensex, it may not trade for all
+             days sensex has worked,, so we take a left join
+        """
+    print df
 
 def test_run():
-    """ my driver function
-    """
+
     start_date = '2014-01-01'
     end_date = '2016-10-07'
-    path = "./Data/SENSEX/TCS.csv"
-    make_dataframe(path, start_date, end_date)
+    dates = pandas.date_range(start_date, end_date)
+    symbols = ['TCS', 'ITC']
+    get_data_frame(symbols, dates)
 
-    """for symbol in ['ITC' , 'TCS']:
-        print "Max Close is"
-        path = "./Data/SENSEX/{}.csv".format(symbol)
-        #print symbol, get_mean_volume(path)
-        plot_values(path)
-    """
 
 if __name__ == "__main__":
     test_run()
